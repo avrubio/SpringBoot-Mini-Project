8c908ada46ebcbd72b840cc96dd0e3e751b69f02
avrubio <ariadna.vanegas@gmail.com>
2023-09-18 17:02:16 -0400

    lombok Getter added for category class

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/Category.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/Category.java
@@ -1,8 +1,11 @@
 package com.example.springbootminiproject.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
