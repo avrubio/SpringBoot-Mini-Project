853a5aa9acbd3585265385f5db2aabe9356fbbde
avrubio <ariadna.vanegas@gmail.com>
2023-09-18 17:01:41 -0400

    lombok No Args, All args contructors added

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/Category.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/Category.java
@@ -1,5 +1,10 @@
 package com.example.springbootminiproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
    private Long id;
     private String name;
