0e62c92b90b80d6b4613b45bd4474e0d71ccc9ae
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 12:53:38 -0400

    Autowired dependency injection added to Category Service

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
@@ -2,10 +2,16 @@ package com.example.springbootminiproject.service;
 
 import com.example.springbootminiproject.repository.CategoryRepository;
 import com.example.springbootminiproject.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
     private CategoryRepository categoryRepository;
     private ProductRepository productRepository;
+
+    @Autowired
+    public void setCategoryRepository(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 }
