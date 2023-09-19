e769972c837a3477ca815574122f8e696d23ff80
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 11:47:18 -0400

    Table annotations added to Product Class

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/Product.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/Product.java
@@ -1,5 +1,7 @@
 package com.example.springbootminiproject.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,9 +9,26 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+    @Column
     private String directions;
 
+    @JsonIgnore
+    @ManyToOne //many recipes belong to a 1 category
+    @JoinColumn(name = "category_id")  //foreign key
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
 }
