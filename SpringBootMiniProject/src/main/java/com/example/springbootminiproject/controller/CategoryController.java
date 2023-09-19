5eba843d69531b35bba2af3d19ae1b7e620d74fa
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 14:38:51 -0400

    getCatgory api endpoint added to CategoryController

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
@@ -4,10 +4,12 @@ import com.example.springbootminiproject.model.Category;
 import com.example.springbootminiproject.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api") // http://localhost:4444/api
@@ -19,9 +21,12 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping(path = "/categories/") // http://localhost:8080/api/categories/
+    @GetMapping(path = "/categories/") // http://localhost:4444/api/categories/
     public List<Category> getCategories() {
         return categoryService.getCategories();
+    }
+    @GetMapping(path = "/categories/{categoryId}/") // http://localhost:4444/api/categories/
+    public Optional<Category> getCategory(@PathVariable(value = "categoryId") Long categoryId)  {return categoryService.getCategory(categoryId);
     }
 
 
