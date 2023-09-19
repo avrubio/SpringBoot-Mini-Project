4fd526c919442b8ab356153f26e5434192da587a
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 12:56:24 -0400

    ResponseStatus annotation added to InformationExistException class

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/expection/InformationExistException.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/expection/InformationExistException.java
@@ -1,4 +1,8 @@
 package com.example.springbootminiproject.expection;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException {
 }
