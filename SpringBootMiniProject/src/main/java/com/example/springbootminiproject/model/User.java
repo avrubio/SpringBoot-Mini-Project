9a231b3398043c48c9d904cbdc3e35721cfe7d5e
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 11:30:34 -0400

    Constructors added to User Class

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/User.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/User.java
@@ -1,4 +1,9 @@
 package com.example.springbootminiproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 }
