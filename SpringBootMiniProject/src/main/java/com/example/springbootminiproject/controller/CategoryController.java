db18271345a5a1f8c3644fc62d05d256a76f932a
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:03:57 -0400

    CategoryService instance field added to CategoryController

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.service.CategoryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api") // http://localhost:4444/api
 public class CategoryController {
+    private CategoryService categoryService;
+
 
 }
