31987047932f7ac17d9a4dfa9c3c6c34f3f7c717
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:02:28 -0400

    setProductRepository dependency bean added

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
@@ -14,4 +14,10 @@ public class CategoryService {
     public void setCategoryRepository(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+
+    @Autowired //to connect to recipe repository and to use its methods
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
 }
