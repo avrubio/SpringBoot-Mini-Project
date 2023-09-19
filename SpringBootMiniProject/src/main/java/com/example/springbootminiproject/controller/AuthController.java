3ab98a3b65968fd0ab8c3ca47d9ff1f0528af7f4
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 14:47:28 -0400

    added mapping and instance field to AuthController

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/AuthController.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/AuthController.java
@@ -1,4 +1,12 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/users/")//http://localhost:4444/auth/users/
 public class AuthController {
+    private UserService userService;
+
 }
