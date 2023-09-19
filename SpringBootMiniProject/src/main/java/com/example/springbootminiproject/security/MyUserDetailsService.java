8982d60c7eea9b6a8fa24a10776780a1a6f318b1
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:22:29 -0400

    UserService instance field added to MyUserDetailsService

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/security/MyUserDetailsService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/security/MyUserDetailsService.java
@@ -1,7 +1,10 @@
 package com.example.springbootminiproject.security;
 
+import com.example.springbootminiproject.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService {
+    private UserService userService;
+
 }
