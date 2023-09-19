bfabd9d0fce803fb3c04d596a6079ccdcc23316a
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:24:13 -0400

    Instance fields added to UserService

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/UserService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/UserService.java
@@ -1,7 +1,16 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.repository.UserRepository;
+import com.example.springbootminiproject.security.JWTUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
 }
