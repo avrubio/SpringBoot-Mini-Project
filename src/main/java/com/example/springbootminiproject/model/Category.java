ab736834efcef579c10261f42e736bbe6ab5fdc2
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 16:41:55 -0400

    javadocs added to category

--- src/main/java/com/example/springbootminiproject/model/Category.java
+++ src/main/java/com/example/springbootminiproject/model/Category.java
@@ -1,6 +1,5 @@
 package com.example.springbootminiproject.model;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.LazyCollection;
@@ -8,6 +7,10 @@ import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
 import java.util.List;
+
+/**
+ * Represents a category in the application.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,26 +20,38 @@ import java.util.List;
 @Table(name = "categories")
 public class Category {
 
+    /**
+     * The unique identifier of the category.
+     */
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The name of the category.
+     */
     @Column
     private String name;
 
+    /**
+     * The description of the category.
+     */
     @Column
     private String description;
 
+    /**
+     * The list of products associated with this category.
+     */
     @OneToMany(mappedBy = "category", orphanRemoval = true)
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Product> productList;
 
+    /**
+     * The user who owns or manages this category.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     @JsonIgnore
     private User user;
-
-
-
 }
