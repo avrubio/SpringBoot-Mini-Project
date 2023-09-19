b9faabde0aed9c1556ecc8ed649e97330b112f6d
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:48:05 -0400

    getCategories method added to CategoryService

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.expection.InformationNotFoundException;
+import com.example.springbootminiproject.model.Category;
 import com.example.springbootminiproject.model.User;
 import com.example.springbootminiproject.repository.CategoryRepository;
 import com.example.springbootminiproject.repository.ProductRepository;
@@ -7,6 +9,9 @@ import com.example.springbootminiproject.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -26,6 +31,15 @@ public class CategoryService {
     public static User getCurrentLoggedInUser(){
         MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userDetails.getUser();
+    }
+
+    public List<Category> getCategories() {
+        List<Category> categoryList = categoryRepository.findByUserId(CategoryService.getCurrentLoggedInUser().getId());
+        if(categoryList.isEmpty()){
+            throw new InformationNotFoundException("no categories found for user id " + CategoryService.getCurrentLoggedInUser().getId());
+        } else {
+            return categoryList;
+        }
     }
 
 
