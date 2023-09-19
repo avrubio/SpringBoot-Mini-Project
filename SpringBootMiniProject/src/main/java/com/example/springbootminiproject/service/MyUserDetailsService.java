3fe96ef7dee8b4d78f051d9125b79333b9c6a790
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 14:55:49 -0400

    loadByUsername method added to MyUserDetailsService

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/MyUserDetailsService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/MyUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.model.User;
+import com.example.springbootminiproject.security.MyUserDetails;
 import com.example.springbootminiproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,5 +16,9 @@ public class MyUserDetailsService {
         this.userService = userService;
     }
 
-
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(emailAddress);
+        return new MyUserDetails(user);
+    }
 }
