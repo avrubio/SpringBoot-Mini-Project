e9a55abea55b99acb38a882e4256fe8a591a1a3e
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:06:15 -0400

    SecurityConfiguration package and class created

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
@@ -1,9 +1,13 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.model.Category;
 import com.example.springbootminiproject.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api") // http://localhost:4444/api
@@ -13,6 +17,11 @@ public class CategoryController {
     @Autowired
     public void setCategoryService(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping(path = "/categories/") // http://localhost:8080/api/categories/
+    public List<Category> getCategories() {
+        return categoryService.getCategories();
     }
 
 
