6d9463dce4d065d15e452ce4eee0329dcb0c47a1
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 11:41:24 -0400

    UserProfile instance fields added

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/UserProfile.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/UserProfile.java
@@ -8,4 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserProfile {
-}
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String profileDescription;
