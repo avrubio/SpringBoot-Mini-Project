9b16b05c4254b697c657adbabc12125eaacd1678
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 16:43:45 -0400

    javadocs added to User class

--- src/main/java/com/example/springbootminiproject/model/User.java
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -8,6 +8,9 @@ import org.hibernate.annotations.LazyCollectionOption;
 import javax.persistence.*;
 import java.util.List;
 
+/**
+ * Represents a user in the application.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,39 +20,64 @@ import java.util.List;
 @Table(name = "users")
 public class User {
 
+    /**
+     * The unique identifier of the user.
+     */
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The username of the user.
+     */
     @Column
     private String userName;
 
+    /**
+     * The email address of the user (unique).
+     */
     @Column(unique = true)
     private String emailAddress;
 
+    /**
+     * The password of the user (access set to WRITE_ONLY for security).
+     */
     @Column
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    /**
+     * The list of categories associated with this user.
+     */
     @OneToMany(mappedBy = "user")
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Category> categoryList;
 
+    /**
+     * The list of products associated with this user.
+     */
     @OneToMany(mappedBy = "user")
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Product> productList;
 
+    /**
+     * The user's profile information.
+     */
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "profile_id", referencedColumnName = "id")
     private UserProfile userProfile;
 
+    /**
+     * Find a category by its ID.
+     *
+     * @param id The ID of the category to find.
+     * @return The found category or null if not found.
+     */
     public Category findCategoryById(Long id) {
         return this.getCategoryList().stream()
                 .filter(category -> category.getId().equals(id))
                 .findFirst()
                 .orElse(null);
     }
-
-
 }
