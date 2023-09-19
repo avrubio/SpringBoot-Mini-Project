848d00182d628ed035e67809051df228fe8522e9
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 16:50:10 -0400

    javadocs added to SecurityConfiguration

--- src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
@@ -11,34 +11,62 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * Configuration class for setting up Spring Security.
+ */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
-
+    /**
+     * Configure the password encoder for secure password storage.
+     *
+     * @return The BCryptPasswordEncoder bean.
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Create a JWT request filter bean.
+     *
+     * @return The JwtRequestFilter bean.
+     */
     @Bean
     public JwtRequestFilter authJwtRequestFilter() {
         return new JwtRequestFilter();
     }
 
+    /**
+     * Configure the security filter chain for handling HTTP requests.
+     *
+     * @param http The HttpSecurity object to configure.
+     * @return The SecurityFilterChain bean.
+     * @throws Exception If there's an error in the configuration.
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login/", "/auth/users/register/").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/auth/users", "/auth/users/login/", "/auth/users/register/").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable()
                 .headers().frameOptions().disable();
+
         http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
+    /**
+     * Configure the AuthenticationManager.
+     *
+     * @param authConfig The AuthenticationConfiguration to obtain the manager.
+     * @return The AuthenticationManager bean.
+     * @throws Exception If there's an error in obtaining the manager.
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
