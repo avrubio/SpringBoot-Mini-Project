d17a2d457d0e28d8dd1edfa4f9209c5ae58f67b1
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 16:42:44 -0400

    javadocs to product class added:

--- src/main/java/com/example/springbootminiproject/model/Product.java
+++ src/main/java/com/example/springbootminiproject/model/Product.java
@@ -4,39 +4,65 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+
+/**
+ * Represents a product in the application.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Entity
-@Table (name = "products")
+@Table(name = "products")
 public class Product {
 
+    /**
+     * The unique identifier of the product.
+     */
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * The name of the product.
+     */
     @Column
     private String name;
+
+    /**
+     * The description of the product.
+     */
     @Column
     private String description;
 
+    /**
+     * The category to which this product belongs.
+     */
     @JsonIgnore
-    @ManyToOne //many recipes belong to a 1 category
-    @JoinColumn(name = "category_id")  //foreign key
+    @ManyToOne // Many products belong to one category.
+    @JoinColumn(name = "category_id")  // Foreign key referencing the category.
     private Category category;
 
+    /**
+     * The user who owns or manages this product.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     @JsonIgnore
     private User user;
 
-    public Product update (Product productObject) {
+    /**
+     * Updates the product with new information.
+     *
+     * @param productObject The product object with updated data.
+     * @return The updated product.
+     */
+    public Product update(Product productObject) {
         this.setName(productObject.getName());
         this.setDescription(productObject.getDescription());
 
         return this;
     }
-
 }
