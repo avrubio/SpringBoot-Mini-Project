95d7a50b1e096afac84cd77ed037e0940a7067bc
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 12:52:13 -0400

    Instance fields added to CategoryService class

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/service/CategoryService.java
@@ -1,7 +1,11 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.repository.CategoryRepository;
+import com.example.springbootminiproject.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+    private CategoryRepository categoryRepository;
+    private ProductRepository productRepository;
 }
