aa493da98171a15c5175eb374453b79417784ecb
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 14:52:56 -0400

    JwtRequestFilter classes, instance fields added

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/security/JwtRequestFilter.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/security/JwtRequestFilter.java
@@ -1,4 +1,16 @@
 package com.example.springbootminiproject.security;
 
+import com.example.springbootminiproject.service.MyUserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
 public class JwtRequestFilter {
+    Logger logger = Logger.getLogger(JwtRequestFilter.class.getName());
+
+
+    private MyUserDetailsService myUserDetailsService;
+    private JWTUtils jwtUtils;
+
 }
