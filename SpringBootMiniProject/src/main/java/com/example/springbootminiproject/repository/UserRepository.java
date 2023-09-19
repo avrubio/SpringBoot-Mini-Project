0f8703bdb109f600c52f4d73b436d7d7f0046edb
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 12:51:20 -0400

    UserRepository extends JpaRepository

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/repository/UserRepository.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.springbootminiproject.repository;
-
+import com.example.springbootminiproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
