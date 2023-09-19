da883eb6b63ed644b30e68c49afe3ebba638b98a
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 16:47:18 -0400

    javadocs added to MyUserDetails

--- src/main/java/com/example/springbootminiproject/security/MyUserDetails.java
+++ src/main/java/com/example/springbootminiproject/security/MyUserDetails.java
@@ -3,23 +3,30 @@ package com.example.springbootminiproject.security;
 import com.example.springbootminiproject.model.User;
 
 import org.springframework.security.core.GrantedAuthority;
-
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.HashSet;
 
-
+/**
+ * Custom UserDetails implementation for representing user details in Spring Security.
+ */
 public class MyUserDetails implements UserDetails {
 
     private final User user;
 
+    /**
+     * Constructs a MyUserDetails instance with the associated user.
+     *
+     * @param user The user for whom the user details are created.
+     */
     public MyUserDetails(User user) {
         this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        // In this example, we return an empty set of granted authorities.
         return new HashSet<>();
     }
 
@@ -53,8 +60,12 @@ public class MyUserDetails implements UserDetails {
         return true;
     }
 
+    /**
+     * Get the associated user.
+     *
+     * @return The associated User object.
+     */
     public User getUser() {
         return user;
     }
-
 }
