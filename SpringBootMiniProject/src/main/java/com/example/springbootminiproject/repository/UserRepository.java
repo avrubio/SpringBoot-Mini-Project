094d456413b3a2ebed5809984a85e69f43a78811
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 13:15:59 -0400

    existsByEmailAddress method added to UserRepository

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/repository/UserRepository.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/repository/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //to register
+    boolean existsByEmailAddress(String userEmailAddress);
+
 }
