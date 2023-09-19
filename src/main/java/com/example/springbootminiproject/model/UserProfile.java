9a7375b42b8d2df6f5a03301fd43ef299460e313
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 16:44:27 -0400

    javadocs addes to UserProfile class

--- src/main/java/com/example/springbootminiproject/model/UserProfile.java
+++ src/main/java/com/example/springbootminiproject/model/UserProfile.java
@@ -4,28 +4,49 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+
+/**
+ * Represents a user's profile in the application.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Entity
-@Table(name="profiles")
+@Table(name = "profiles")
 public class UserProfile {
+
+    /**
+     * The unique identifier of the user's profile.
+     */
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * The first name of the user.
+     */
     @Column
     private String firstName;
+
+    /**
+     * The last name of the user.
+     */
     @Column
     private String lastName;
+
+    /**
+     * The description of the user's profile.
+     */
     @Column
     private String profileDescription;
 
+    /**
+     * The user associated with this profile.
+     */
     @JsonIgnore
     @OneToOne(mappedBy = "userProfile")
     private User user;
-
-
 }
