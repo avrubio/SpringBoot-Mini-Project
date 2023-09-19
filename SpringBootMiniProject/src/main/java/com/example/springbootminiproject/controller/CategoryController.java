7c0a32ecea010ef01bc7d567fa9d02375d0643de
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 11:58:40 -0400

    RestController annotation added to category controller class, and Request Mapping for url path to api

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/CategoryController.java
@@ -1,5 +1,10 @@
 package com.example.springbootminiproject.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // http://localhost:4444/api
 public class CategoryController {
 
 }
