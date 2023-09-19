a2aee5d3c10c6bc022faf73bcb096858ed69b3bd
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 11:49:14 -0400

    Table annotations added to User class

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/User.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/model/User.java
@@ -1,15 +1,42 @@
 package com.example.springbootminiproject.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String userName;
+    @Column
     private String emailAddress;
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Category> categoryList;
+
+    @OneToMany(mappedBy = "user")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Product> productList;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    private UserProfile userProfile;
 }
