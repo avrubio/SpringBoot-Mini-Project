6afded5fa0d7fffbade03ba1fe42d433ef1e2c90
avrubio <ariadna.vanegas@gmail.com>
2023-09-19 14:48:02 -0400

    setUserSercice and createUser added to AuthController

--- SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/AuthController.java
+++ SpringBootMiniProject/src/main/java/com/example/springbootminiproject/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.model.User;
 import com.example.springbootminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/auth/users/")//http://localhost:4444/auth/users/
 public class AuthController {
     private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping(path="/register/")
+    public User createUser(@RequestBody User userObject){
+        return userService.createUser(userObject);
+    }
 
 }
